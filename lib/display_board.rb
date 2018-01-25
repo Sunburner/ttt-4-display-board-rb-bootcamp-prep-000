dc65d7eede15b1c8ecfe6225f37ecc561f2ea606
Angelo Cigna <acigna1982@gmail.com>
2018-01-25 18:57:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
+def display_board
 
-end
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+
+
+
+end #end display_board
+
